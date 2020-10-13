e5f3cf83b65e69f2d2129d98c09216c82070619b
Jesse Fu <jesse920524@163.com>
2020-10-13 18:00:07 +0800

    完成练习06-14

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -17,6 +18,7 @@ public class Practice12CameraRotateFixedView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -37,8 +39,31 @@ public class Practice12CameraRotateFixedView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float cx = 200 + bitmap.getWidth()/2;
+        float cy = 200 + bitmap.getHeight()/2;
+        float cx1 = 600 + bitmap.getWidth()/2;
+        float cy1 = 200 + bitmap.getHeight()/2;
 
+        canvas.save();
+        camera.save();
+        camera.rotateX(30);
+
+        canvas.translate(cx, cy);//旋转后把投影移动回来
+        camera.applyToCanvas(canvas);
+        camera.restore();
+        canvas.translate(-cx, -cy);//旋转前把view中心移动到原点
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(30);
+
+        canvas.translate(cx1, cy1);
+        camera.applyToCanvas(canvas);
+        camera.restore();
+        canvas.translate(-cx1, -cy1);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
